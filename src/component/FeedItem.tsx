308fc4e3f614cad0e661bdac49fbb996e6c4283a
Seongho <bluetosh7@gmail.com>
2024-03-20 14:13:12 +0900

    tmp

---
+++ src/component/FeedItem.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export interface ContainerProps extends React.ComponentPropsWithRef<"div"> {}
+
+export const Container = React.forwardRef<HTMLDivElement, ContainerProps>(
+  (props, ref) => {
+    return <div {...props} ref={ref}></div>;
+  }
+);
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({
+  primary = false,
+  size = "medium",
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
+  return (
+    <button
+      type="button"
+      className={["storybook-button", `storybook-button--${size}`, mode].join(
+        " "
+      )}
+      {...props}
+    >
+      {label}
+      <style jsx>{`
+        button {
+          background-color: ${backgroundColor};
+        }
+      `}</style>
+    </button>
+  );
+};
